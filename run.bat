ce060ead4019688ab3da301c72e368538f7a76a8
ankit0706 <46675158+ankit0706@users.noreply.github.com>
2020-06-20 16:34:20 +0530

    maven project with executable jar
    Tested okay

---
+++ run.bat
@@ -0,0 +1,3 @@
+set JAVA_HOME=C:\Program Files\Java\jdk1.8.0_144
+set PATH=%PATH%;C:\Users\AANU\maven\bin
+mvn -Dmaven.test.skip=true clean install
---
+++ src/main/java/sample/randomid/MainApp.java
@@ -0,0 +1,32 @@
+package sample.randomid;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.charset.Charset;
+import java.util.UUID;
+
+import org.apache.commons.io.FileUtils;
+
+public class MainApp {
+	public static void main(String[] args) {
+		File file = new File("id-set.txt");
+		MainApp app = new MainApp();
+		app.createFile(file, 100);
+	}
+	
+	
+	private void createFile(File file, int count){
+		String content = "";
+		StringBuilder builder = new StringBuilder();
+		for(int i = 0; i < count; i++) {
+			String guid = UUID.randomUUID().toString();	
+			builder.append(guid + "\n");
+		}
+		try {
+			content = builder.toString();
+			FileUtils.writeStringToFile(file, content, Charset.defaultCharset());
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+}
